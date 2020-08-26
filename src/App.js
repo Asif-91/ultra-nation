f690a73bd5dd9ff88f100850ba7e4a3d60ffe94c
Asif-91 <asifchowdhury323@gmail.com>
2020-08-26 23:14:31 +0600

    ultra nation finishesd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data=>setCountries(data))
+  }, [])
+  
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart); 
+  }
+
   return (
     <div className="App">
+      <h1>country loaded:{countries.length}</h1>
+      <h4>country added: {cart.length}</h4>
+      <Cart cart={cart} ></Cart>
+      <ul>
+        {
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -14,7 +38,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noOpener noReferrer"
         >
           Learn React
         </a>
